a2fa6955f850dab4ad1b603d0e2927f164d5f834
Hawa <hawamaria33@gmail.com>
2023-06-11 14:55:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left, 10);
+
+  if (left < window.innerWidth - 40) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
